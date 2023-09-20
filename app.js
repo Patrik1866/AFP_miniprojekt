a6701cd815f01b140fe38a6e47693302d65998a1
Erdélyi Patrik <erdelyi.patrik99@gmail.com>
2023-09-20 17:49:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ const express = require('express');
 const path = require('path');
 // Creating express object
 const app = express();
-app.use(express.static(path.join(__dirname, 'AFP1_miniproject')));
+app.use(express.static(path.join(__dirname, 'Public')));
 
 
 // Handling GET request
 app.get('/', async (req, res) => {
-	res.sendFile(path.join(__dirname, 'AFP1_miniproject', 'Home.html'));
+	res.sendFile(path.join(__dirname, 'Public', 'Home.html'));
 });
 
 // Port Number
-const PORT = process.env.PORT ||5000;
+const PORT = process.env.PORT ||4000;
  
 // Server Setup
 app.listen(PORT,console.log(

4b162238f656811adae224f76f7cc34e8a68f252
Erdélyi Patrik <erdelyi.patrik99@gmail.com>
2023-10-01 18:57:58 +0200

    Adatbázis, Bejelentkezés, Regisztráció
    Az app.js-ben történt a legtöbb változás amik a következők:-Adatbázis kapcsolat kiépítése
    -regisztráció az adatbázison keresztül, illetve bejelentkezés adatbázison keresztül

--- app.js
+++ app.js
@@ -1,22 +1,79 @@
-const exp = require('constants');
 const express = require('express');
- 
-
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 const path = require('path');
-// Creating express object
 const app = express();
-app.use(express.static(path.join(__dirname, 'Public')));
 
+// Create a MySQL database connection
+const db = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  database: 'viccportal_db',
+});
 
-// Handling GET request
 app.get('/', async (req, res) => {
 	res.sendFile(path.join(__dirname, 'Public', 'Home.html'));
 });
 
-// Port Number
-const PORT = process.env.PORT ||4000;
- 
-// Server Setup
-app.listen(PORT,console.log(
-`Server started on port ${PORT}`));
+// Connect to the database
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Connected to MySQL');
+});
 
+// Serve static files (HTML and CSS)
+app.use(express.static('Public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function displayPopupMessage(message) {
+	console.log('\x1b[36m%s\x1b[0m', '== Popup Message =='); // Add styling (optional)
+	console.log(message);
+	console.log('===================');
+  }
+// Handle form submissions
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+
+  // Insert the form data into the database
+  const sql = 'INSERT INTO viccportal (username, password) VALUES (?, ?)';
+  db.query(sql, [username, password], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error registering user');
+    } else {
+      console.log('User registered successfully');
+      res.redirect('Home.html');
+    }
+	
+  });
+});
+
+app.post('/login', (req, res) => {
+	const { username, password } = req.body;
+  
+	// Query the database to check if the user exists
+	const sql = 'SELECT * FROM viccportal WHERE username = ? AND password = ?';
+	db.query(sql, [username, password], (err, results) => {
+	  if (err) {
+		console.error(err);
+		res.status(500).send('Internal server error');
+	  } else if (results.length === 0) {
+		res.status(401).send('Authentication failed'); // User not found or incorrect password
+	  } else {
+		// User is authenticated
+		console.log('User login successful')
+		res.redirect('MainPage.html')
+	  }
+	});
+  });
+
+// Start the server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
